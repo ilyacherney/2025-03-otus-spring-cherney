f14335ab50bd70981e625937bb7df8d5bd2feb8a
Ilya Cherney <ilyacherney@yandex.ru>
2025-05-24 16:31:10 +0300

    add convertation from csv to question

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,11 +1,21 @@
 package ru.otus.hw.dao;
 
-import com.opencsv.bean.CsvToBean;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvException;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+import ru.otus.hw.util.FileResourcesUtils;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +23,34 @@ import java.util.List;
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
+    @CsvBindAndSplitByName(elementType = Question.class)
+    private List<Question> questions;
+
     @Override
     public List<Question> findAll() {
+
         // todo Использовать CsvToBean
         //  https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        FileResourcesUtils utils = new FileResourcesUtils();
+        CSVReader c = new CSVReaderBuilder(new InputStreamReader(utils.getFileFromResourceAsStream(fileNameProvider.getTestFileName())))
+                .withCSVParser(new CSVParserBuilder()
+                        .withSeparator(';')
+                        .build())
+                .withSkipLines(1)
+                .build();
+
+        CsvToBean<Question> csvToBean = new CsvToBeanBuilder(c).withType(Question.class).withMappingStrategy(new M)build();
+
+        List<Question> questionList;
+
+
+        questionList = csvToBean.parse();
 
         // todo Использовать QuestionReadException
-        //  Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+            //  Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        // todo убрать хардкод
+            // todo убрать хардкод
+
         return List.of(new Question("Как дела?", List.of(new Answer("Супер", true))));
     }
 }
