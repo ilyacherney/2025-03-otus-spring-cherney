f186022b31950e91d3991dd85386dcf8bad35a57
Ilya Cherney <ilyacherney@yandex.ru>
2025-05-24 14:38:15 +0300

    Add questions

--- hw01/src/main/java/ru/otus/hw/Application.java
+++ hw01/src/main/java/ru/otus/hw/Application.java
@@ -7,7 +7,7 @@ import ru.otus.hw.service.TestRunnerService;
 public class Application {
     public static void main(String[] args) {
         // todo [DONE] Прописать бины в spring-context.xml и создать контекст на его основе
-        // todo: Добавить свои вопросы в questions.txt
+        // todo [DONE]: Добавить свои вопросы в questions.txt
         // todo: Пропустить строку в questions.txt
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         var testRunnerService = context.getBean(TestRunnerService.class);
