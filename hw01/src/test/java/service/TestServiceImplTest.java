c5a49abd618fb34a53e14ed75056d05e6c85b40d
Ilya Cherney <ilyacherney@yandex.ru>
2025-05-25 22:58:39 +0300

    add unit-test method

--- hw01/src/test/java/service/TestServiceImplTest.java
+++ hw01/src/test/java/service/TestServiceImplTest.java
@@ -1,11 +1,40 @@
 package service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.service.TestService;
+import org.mockito.*;
+import ru.otus.hw.service.TestServiceImpl;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 public class TestServiceImplTest  {
 
-    public void testExecuteTest() {
+    @Mock
+    QuestionDao questionDao;
 
+    @InjectMocks
+    TestServiceImpl testServiceImpl;
+
+    @Test
+    public void shouldPrintReceivedQuestions() {
+        List<Question> questions = List.of(
+                new Question("How many planets are in the Solar System?",
+                List.of(
+                        new Answer("There are 8 planet", true),
+                        new Answer("There are 9 planets", false))));
+
+        given(questionDao.findAll()).willReturn(questions);
+
+        testServiceImpl.executeTest();
+
+//        verify(testServiceImpl).
     }
 
 }
