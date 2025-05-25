dc16c14cdbe37720db5aa73570acab095d4277d3
Ilya Cherney <ilyacherney@yandex.ru>
2025-05-25 12:57:29 +0300

    start unit-test

--- hw01/src/main/java/ru/otus/hw/Application.java
+++ hw01/src/main/java/ru/otus/hw/Application.java
@@ -9,10 +9,10 @@ public class Application {
         // todo [DONE] Прописать бины в spring-context.xml и создать контекст на его основе
         // todo [DONE]: Добавить свои вопросы в questions.txt
         // todo [DONE]: Пропустить строку в questions.txt
-        // todo: Все зависимости должны быть настроены в IoC контейнере
         // todo: Необходимо написать юнит-тест сервиса тестирования
-        // todo: Проверка checkstyle должна проходить успешно
+        // todo: Все зависимости должны быть настроены в IoC контейнере
         // todo: причесать всё
+        // todo: Проверка checkstyle должна проходить успешно
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
---
+++ hw01/src/test/java/service/TestServiceImplTest.java
@@ -0,0 +1,11 @@
+package service;
+
+import ru.otus.hw.service.TestService;
+
+public class TestServiceImplTest  {
+
+    public void testExecuteTest() {
+
+    }
+
+}
