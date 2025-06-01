42d206212015fdadd0622047ef4826c89bde3d13
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-01 13:50:20 +0300

    add todos

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,6 +1,8 @@
 package ru.otus.hw.repositories;
 
+import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
@@ -11,11 +13,14 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@AllArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcTemplate jdbc;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return jdbc.query("select id, full_name from authors", new AuthorRowMapper());
     }
 
     @Override
@@ -27,7 +32,9 @@ public class JdbcAuthorRepository implements AuthorRepository {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            long id = rs.getLong("id");
+            String fullName = rs.getString("full_name");
+            return new Author(id, fullName);
         }
     }
 }
