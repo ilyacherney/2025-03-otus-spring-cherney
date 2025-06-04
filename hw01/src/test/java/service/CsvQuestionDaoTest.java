8a5ccd61e6db5dc09bb434bd19ed8b033370c3d1
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-04 18:18:04 +0300

    ьmodify CsvQuestionDaoTest

--- hw01/src/test/java/service/CsvQuestionDaoTest.java
+++ hw01/src/test/java/service/CsvQuestionDaoTest.java
@@ -1,12 +1,22 @@
 package service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.util.Assert;
 import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.service.TestDataProvider;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 public class CsvQuestionDaoTest {
 
@@ -23,6 +33,13 @@ public class CsvQuestionDaoTest {
 
     @Test
     void shouldFindQuestionsFromExistingSource() {
+        String testFileName = "test-questions.csv";
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(testFileName);
 
+        given(testDataProvider.provideTestData()).willReturn(inputStream);
+
+        List<Question> questions = csvQuestionDao.findAll();
+
+        Assert.notEmpty(questions, "There should be at least one question");
     }
 }
