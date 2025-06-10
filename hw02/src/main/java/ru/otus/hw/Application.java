655c4a3df32852274071881307c4612689de0b79
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-10 19:27:07 +0300

    Add annotation-based configuration

--- hw02/src/main/java/ru/otus/hw/Application.java
+++ hw02/src/main/java/ru/otus/hw/Application.java
@@ -1,15 +1,17 @@
 package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.otus.hw.service.TestRunnerService;
 
 public class Application {
     public static void main(String[] args) {
 
         // todo [ ]: Создать контекст на основе Annotation/Java конфигурирования
-        ApplicationContext context = null;
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.refresh();
         var testRunnerService = context.getBean(TestRunnerService.class);
-        testRunnerService.run();
+//        testRunnerService.run();
 
         //  todo [ ]: добавить вопросы в questions.csv
     }
