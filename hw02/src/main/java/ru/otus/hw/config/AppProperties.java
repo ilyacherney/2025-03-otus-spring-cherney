57e1a0cf7ddeee108283e92afc06584563518afe
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-12 15:33:14 +0300

    inject values to AppProperties from application.properties

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,15 +2,19 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
+@PropertySource("classpath:application.properties")
 @Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // todo [ ]: внедрить свойство из application.properties
+    @Value("${test.rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
     // todo [ ]: внедрить свойство из application.properties
+    @Value("${test.fileName}")
     private String testFileName;
 }
