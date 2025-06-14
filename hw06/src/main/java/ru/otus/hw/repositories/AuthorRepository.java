085775ebc112e0a4b8ab799422e5841e6ef2342e
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-14 12:39:24 +0300

    Implement author`s repository with JPA

--- hw06/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++ hw06/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -1,11 +1,14 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AuthorRepository {
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
     List<Author> findAll();
 
     Optional<Author> findById(long id);
