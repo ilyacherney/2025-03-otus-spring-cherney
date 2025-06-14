ca963f2956724b1fbe111b985d11351f20dcc5b4
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-14 15:35:27 +0300

    Implement GenreRepositoryJpa.findAll with JPA

--- hw06/src/main/java/ru/otus/hw/repositories/GenreRepository.java
+++ hw06/src/main/java/ru/otus/hw/repositories/GenreRepository.java
@@ -1,12 +1,18 @@
 package ru.otus.hw.repositories;
 
+import jakarta.persistence.PersistenceContext;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GenreRepository {
+//
+//    // todo: это реализация Spring Data JPA, а нужно просто Spring JPA
     List<Genre> findAll();
-
+//
     Optional<Genre> findById(long id);
 }
---
+++ hw06/src/main/java/ru/otus/hw/repositories/GenreRepositoryJpa.java
@@ -0,0 +1,28 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class GenreRepositoryJpa implements GenreRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Genre> findAll() {
+        return em.createQuery("SELECT g FROM genres g", Genre.class).getResultList();
+    }
+
+    @Override
+    public Optional<Genre> findById(long id) {
+        return Optional.empty();
+    }
+
+}
