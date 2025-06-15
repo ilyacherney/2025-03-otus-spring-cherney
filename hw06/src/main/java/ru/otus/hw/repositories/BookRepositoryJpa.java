c30ebfd884544060bdb4624ba03221985427bd8c
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-15 11:52:05 +0300

    Upd book repo

--- hw06/src/main/java/ru/otus/hw/repositories/BookRepositoryJpa.java
+++ hw06/src/main/java/ru/otus/hw/repositories/BookRepositoryJpa.java
@@ -1,17 +1,14 @@
 package ru.otus.hw.repositories;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
 public class BookRepositoryJpa implements BookRepository{
 
     @PersistenceContext
@@ -42,6 +39,7 @@ public class BookRepositoryJpa implements BookRepository{
 
     @Override
     public void deleteById(long id) {
-
+        Book book = em.find(Book.class, id);
+        em.remove(book);
     }
 }
