1209f8d67e5bc228fcb16034e5c144f83549ceb5
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-16 19:39:05 +0300

    add @NoArgsConstructor to AppProperties

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,9 +2,11 @@ package ru.otus.hw.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AppProperties implements TestConfig, TestFileNameProvider {
     private int rightAnswersCountToPass;
 
