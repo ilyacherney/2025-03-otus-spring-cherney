ab6838c364fd203872b665cc32d442bdcc431e80
Ilya Cherney <ilyacherney@yandex.ru>
2025-06-29 08:54:47 +0300

    Init hw03

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -1,15 +1,17 @@
 package ru.otus.hw;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.otus.hw.service.TestRunnerService;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
-        //Создать контекст Spring Boot приложения
-        ApplicationContext context = null;
+        SpringApplication.run(Application.class, args);
+        ApplicationContext context = new AnnotationConfigApplicationContext();
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
-
     }
 }
